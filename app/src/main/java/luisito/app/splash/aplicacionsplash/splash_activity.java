0ae512b5aa672fc0f5e387d7ec6823af5da2d6a4
Luisito <metalico_m4@hotmail.com>
2018-06-09 19:40:38 -0500

    Splash Terminado

---
+++ app/src/main/java/luisito/app/splash/aplicacionsplash/splash_activity.java
@@ -0,0 +1,31 @@
+package luisito.app.splash.aplicacionsplash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class splash_activity extends Activity {
+
+    private final int DURACION_SPLASH = 3000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_activity);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(splash_activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
